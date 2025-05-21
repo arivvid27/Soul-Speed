74713c37c06c78635f8adb8f4a4ff7ebfb74ff20
Videsh Arivazhagan <arivvid27@gmail.com>
2025-05-21 09:23:07 -0700

    Fabric Template Made

---
+++ src/client/java/com/soulspeed113/SoulSpeedClient.java
@@ -0,0 +1,10 @@
+package com.soulspeed113;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SoulSpeedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
