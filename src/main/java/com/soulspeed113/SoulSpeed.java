6913086cd319cd24f1b2c83d2e46e4c627b56905
Videsh Arivazhagan <arivvid27@gmail.com>
2025-05-25 09:21:20 -0700

    attempts flopped

--- src/main/java/com/soulspeed113/SoulSpeed.java
+++ src/main/java/com/soulspeed113/SoulSpeed.java
@@ -3,6 +3,7 @@ package com.soulspeed113;
 import com.soulspeed113.effect.ModEffects;
 import com.soulspeed113.item.ModItems;
 import com.soulspeed113.potion.ModPotions;
+import com.soulspeed113.potion.PotionRecipes;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,10 +17,11 @@ public class SoulSpeed implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("Initializing Soul Speed Potion Mod");
 
-		// CUstom stuff
+		// Custom stuff
 		ModEffects.registerEffects();
 		ModPotions.registerPotions();
 		ModItems.registerItems();
+		PotionRecipes.registerPotionRecipe();
 
 		LOGGER.info("Initialization of SoulSpeed complete");
 	}
