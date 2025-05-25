215638af8bf33ca91a8465b987507e4989829b3f
Videsh Arivazhagan <arivvid27@gmail.com>
2025-05-25 09:09:48 -0700

    good lord i got a file to work

--- src/main/java/com/soulspeed113/potion/ModPotions.java
+++ src/main/java/com/soulspeed113/potion/ModPotions.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.potion.Potion;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
 
 public class ModPotions {
@@ -18,27 +19,19 @@ public class ModPotions {
         SOUL_SPEED = Registry.register(
             Registries.POTION,
             Identifier.of(SoulSpeed.MOD_ID, "soul_speed"),
-            new Potion(
-                StatusEffectInstance(ModEffects.SOUL_SPEED, 3600, 0)
-            )
+            new Potion("soul_speed", new StatusEffectInstance(RegistryEntry.of(ModEffects.SOUL_SPEED), 3600, 0))
         );
-        
+
         LONG_SOUL_SPEED = Registry.register(
             Registries.POTION,
             Identifier.of(SoulSpeed.MOD_ID, "long_soul_speed"),
-            new Potion(
-                StatusEffectInstance(ModEffects.SOUL_SPEED, 9600, 0)
-            )
+            new Potion("long_soul_speed", new StatusEffectInstance(RegistryEntry.of(ModEffects.SOUL_SPEED), 9600, 0))
         );
 
         STRONG_SOUL_SPEED = Registry.register(
             Registries.POTION,
             Identifier.of(SoulSpeed.MOD_ID, "strong_soul_speed"),
-            new Potion(
-                StatusEffectInstance(ModEffects.SOUL_SPEED, 1800, 1)
-            )
+            new Potion("strong_soul_speed", new StatusEffectInstance(RegistryEntry.of(ModEffects.SOUL_SPEED), 1800, 1))
         );
-
-        PotionRecipes.registerPotionRecipe();
     }
 }
