f7c82142bc99574ee51d29f25e59a3db51a81559
Videsh Arivazhagan <arivvid27@gmail.com>
2025-05-25 10:58:04 -0700

    pray for my sanity

--- src/main/java/com/soulspeed113/mixin/PiglinBarterMixin.java
+++ src/main/java/com/soulspeed113/mixin/PiglinBarterMixin.java
@@ -1,26 +1,26 @@
-package com.soulspeed113.mixin;
+// package com.soulspeed113.mixin;
 
-import com.soulspeed113.potion.ModPotions;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.PotionContentsComponent;
-import net.minecraft.entity.mob.PiglinEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.loot.context.LootContext;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+// import com.soulspeed113.potion.ModPotions;
+// import net.minecraft.component.DataComponentTypes;
+// import net.minecraft.component.type.PotionContentsComponent;
+// import net.minecraft.entity.mob.PiglinEntity;
+// import net.minecraft.item.ItemStack;
+// import net.minecraft.item.Items;
+// import net.minecraft.loot.context.LootContext;
+// import org.spongepowered.asm.mixin.Mixin;
+// import org.spongepowered.asm.mixin.injection.At;
+// import org.spongepowered.asm.mixin.injection.Inject;
+// import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PiglinEntity.class)
-public class PiglinBarterMixin {
-    @Inject(method = "getBarterItem", at = @At("RETURN"), cancellable = true)
-    private void addSoulSpeedPotions(LootContext lootContext, CallbackInfoReturnable<ItemStack> cir) {
-        if (lootContext.getRandom().nextFloat() < 0.05F) {
-            ItemStack soulSpeedPotion = new ItemStack(Items.POTION);
-            soulSpeedPotion.set(DataComponentTypes.POTION_CONTENTS, 
-                new PotionContentsComponent(ModPotions.SOUL_SPEED));
-            cir.setReturnValue(soulSpeedPotion);
-        }
-    }
-}
+// @Mixin(PiglinEntity.class)
+// public class PiglinBarterMixin {
+//     @Inject(method = "getBarterItem", at = @At("RETURN"), cancellable = true)
+//     private void addSoulSpeedPotions(LootContext lootContext, CallbackInfoReturnable<ItemStack> cir) {
+//         if (lootContext.getRandom().nextFloat() < 0.05F) {
+//             ItemStack soulSpeedPotion = new ItemStack(Items.POTION);
+//             soulSpeedPotion.set(DataComponentTypes.POTION_CONTENTS, 
+//                 new PotionContentsComponent(ModPotions.SOUL_SPEED));
+//             cir.setReturnValue(soulSpeedPotion);
+//         }
+//     }
+// }
