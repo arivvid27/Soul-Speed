6e09d8cea3fb1cce3bd9eea37260cdea306734a1
Videsh Arivazhagan <arivvid27@gmail.com>
2025-05-25 11:43:43 -0700

    im cancelling my mixins

--- src/main/java/com/soulspeed113/mixin/EntityMixin.java
+++ src/main/java/com/soulspeed113/mixin/EntityMixin.java
@@ -1,63 +1,63 @@
-package com.soulspeed113.mixin;
+// package com.soulspeed113.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+// import org.spongepowered.asm.mixin.Mixin;
+// import org.spongepowered.asm.mixin.injection.At;
+// import org.spongepowered.asm.mixin.injection.Inject;
+// import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.soulspeed113.effect.ModEffects;
+// import com.soulspeed113.effect.ModEffects;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.util.math.BlockPos;
+// import net.minecraft.block.BlockState;
+// import net.minecraft.entity.Entity;
+// import net.minecraft.entity.LivingEntity;
+// import net.minecraft.entity.effect.StatusEffectInstance;
+// import net.minecraft.registry.Registries;
+// import net.minecraft.registry.tag.BlockTags;
+// import net.minecraft.util.math.BlockPos;
 
-@Mixin(Entity.class)
-public class EntityMixin {
-	@Inject(method = "tick", at = @At("HEAD"))
-	private void applySoulSpeedFromPotion(CallbackInfo ci) {
-		Entity entity = (Entity)(Object)this;
+// @Mixin(Entity.class)
+// public class EntityMixin {
+// 	@Inject(method = "tick", at = @At("HEAD"))
+// 	private void applySoulSpeedFromPotion(CallbackInfo ci) {
+// 		Entity entity = (Entity)(Object)this;
 
-		if (!(entity instanceof LivingEntity livingEntity)) {
-			return;
-		}
+// 		if (!(entity instanceof LivingEntity livingEntity)) {
+// 			return;
+// 		}
 
-		// Check if we have the soul speed effect
-		var soulSpeedEntry = Registries.STATUS_EFFECT.getEntry(ModEffects.SOUL_SPEED);
-		StatusEffectInstance effect = livingEntity.getStatusEffect(soulSpeedEntry);
+// 		// Check if we have the soul speed effect
+// 		var soulSpeedEntry = Registries.STATUS_EFFECT.getEntry(ModEffects.SOUL_SPEED);
+// 		StatusEffectInstance effect = livingEntity.getStatusEffect(soulSpeedEntry);
 		
-		if (effect == null) {
-			return;
-		}
+// 		if (effect == null) {
+// 			return;
+// 		}
 
-		int soulSpeedLevel = effect.getAmplifier() + 1;
-		BlockPos pos = entity.getBlockPos();
-		BlockState blockState = entity.getWorld().getBlockState(pos);
+// 		int soulSpeedLevel = effect.getAmplifier() + 1;
+// 		BlockPos pos = entity.getBlockPos();
+// 		BlockState blockState = entity.getWorld().getBlockState(pos);
 
-		if (blockState.isIn(BlockTags.SOUL_SPEED_BLOCKS)) {
-			float multiplier = 0.05F * soulSpeedLevel;
+// 		if (blockState.isIn(BlockTags.SOUL_SPEED_BLOCKS)) {
+// 			float multiplier = 0.05F * soulSpeedLevel;
 
-			if (entity.isOnGround() && (Math.abs(entity.getVelocity().x) > 0.01 || Math.abs(entity.getVelocity().z) > 0.01)) {
-				entity.setVelocity(
-					entity.getVelocity().x * (1.0 + multiplier),
-					entity.getVelocity().y,
-					entity.getVelocity().z * (1.0 + multiplier)
-				);
+// 			if (entity.isOnGround() && (Math.abs(entity.getVelocity().x) > 0.01 || Math.abs(entity.getVelocity().z) > 0.01)) {
+// 				entity.setVelocity(
+// 					entity.getVelocity().x * (1.0 + multiplier),
+// 					entity.getVelocity().y,
+// 					entity.getVelocity().z * (1.0 + multiplier)
+// 				);
 
-				// Add soul particles occasionally
-				if (entity.getRandom().nextFloat() < 0.2F) {
-					entity.getWorld().addParticle(
-						net.minecraft.particle.ParticleTypes.SOUL,
-						entity.getX(),
-						entity.getY() + 0.1,
-						entity.getZ(),
-						0.0, 0.1, 0.0
-					);
-				}
-			}
-		}
-	}
-}
+// 				// Add soul particles occasionally
+// 				if (entity.getRandom().nextFloat() < 0.2F) {
+// 					entity.getWorld().addParticle(
+// 						net.minecraft.particle.ParticleTypes.SOUL,
+// 						entity.getX(),
+// 						entity.getY() + 0.1,
+// 						entity.getZ(),
+// 						0.0, 0.1, 0.0
+// 					);
+// 				}
+// 			}
+// 		}
+// 	}
+// }
--- src/main/java/com/soulspeed113/mixin/LootTableMixin.java
+++ src/main/java/com/soulspeed113/mixin/LootTableMixin.java
@@ -1,59 +1,59 @@
-package com.soulspeed113.mixin;
+// package com.soulspeed113.mixin;
 
-import com.soulspeed113.potion.ModPotions;
-import net.minecraft.component.DataComponentTypes;
-import net.minecraft.component.type.PotionContentsComponent;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.loot.LootTable;
-import net.minecraft.loot.context.LootContext;
-import net.minecraft.util.Identifier;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+// import com.soulspeed113.potion.ModPotions;
+// import net.minecraft.component.DataComponentTypes;
+// import net.minecraft.component.type.PotionContentsComponent;
+// import net.minecraft.item.ItemStack;
+// import net.minecraft.item.Items;
+// import net.minecraft.loot.LootTable;
+// import net.minecraft.loot.context.LootContext;
+// import net.minecraft.util.Identifier;
+// import org.spongepowered.asm.mixin.Mixin;
+// import org.spongepowered.asm.mixin.Shadow;
+// import org.spongepowered.asm.mixin.injection.At;
+// import org.spongepowered.asm.mixin.injection.Inject;
+// import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
+// import java.util.List;
 
-@Mixin(LootTable.class)
-public class LootTableMixin {
-    @Shadow
-    private Identifier id;
+// @Mixin(LootTable.class)
+// public class LootTableMixin {
+//     @Shadow
+//     private Identifier id;
 
-    private static final Identifier BASTION_TREASURE = Identifier.of("minecraft", "chests/bastion_treasure");
-    private static final Identifier RUINED_PORTAL = Identifier.of("minecraft", "chests/ruined_portal");
-    private static final Identifier NETHER_BRIDGE = Identifier.of("minecraft", "chests/nether_bridge");
+//     private static final Identifier BASTION_TREASURE = Identifier.of("minecraft", "chests/bastion_treasure");
+//     private static final Identifier RUINED_PORTAL = Identifier.of("minecraft", "chests/ruined_portal");
+//     private static final Identifier NETHER_BRIDGE = Identifier.of("minecraft", "chests/nether_bridge");
 
-    @Inject(method = "generateLoot", at = @At("RETURN"))
-    private void addSoulSpeedPotionToLoot(LootContext context, CallbackInfoReturnable<List<ItemStack>> cir) {
-        if (this.id == null) return;
+//     @Inject(method = "generateLoot", at = @At("RETURN"))
+//     private void addSoulSpeedPotionToLoot(LootContext context, CallbackInfoReturnable<List<ItemStack>> cir) {
+//         if (this.id == null) return;
         
-        // Only add to specific loot tables
-        if (!this.id.equals(BASTION_TREASURE) && 
-            !this.id.equals(RUINED_PORTAL) && 
-            !this.id.equals(NETHER_BRIDGE)) {
-            return;
-        }
+//         // Only add to specific loot tables
+//         if (!this.id.equals(BASTION_TREASURE) && 
+//             !this.id.equals(RUINED_PORTAL) && 
+//             !this.id.equals(NETHER_BRIDGE)) {
+//             return;
+//         }
 
-        List<ItemStack> loot = cir.getReturnValue();
+//         List<ItemStack> loot = cir.getReturnValue();
 
-        if (context.getRandom().nextFloat() < 0.15F) {
-            float potionTypeRoll = context.getRandom().nextFloat();
-            ItemStack potionStack = new ItemStack(Items.POTION);
+//         if (context.getRandom().nextFloat() < 0.15F) {
+//             float potionTypeRoll = context.getRandom().nextFloat();
+//             ItemStack potionStack = new ItemStack(Items.POTION);
             
-            if (potionTypeRoll < 0.6F) {
-                potionStack.set(DataComponentTypes.POTION_CONTENTS, 
-                    new PotionContentsComponent(ModPotions.SOUL_SPEED));
-            } else if (potionTypeRoll < 0.9F) {
-                potionStack.set(DataComponentTypes.POTION_CONTENTS, 
-                    new PotionContentsComponent(ModPotions.LONG_SOUL_SPEED));
-            } else {
-                potionStack.set(DataComponentTypes.POTION_CONTENTS, 
-                    new PotionContentsComponent(ModPotions.STRONG_SOUL_SPEED));
-            }
+//             if (potionTypeRoll < 0.6F) {
+//                 potionStack.set(DataComponentTypes.POTION_CONTENTS, 
+//                     new PotionContentsComponent(ModPotions.SOUL_SPEED));
+//             } else if (potionTypeRoll < 0.9F) {
+//                 potionStack.set(DataComponentTypes.POTION_CONTENTS, 
+//                     new PotionContentsComponent(ModPotions.LONG_SOUL_SPEED));
+//             } else {
+//                 potionStack.set(DataComponentTypes.POTION_CONTENTS, 
+//                     new PotionContentsComponent(ModPotions.STRONG_SOUL_SPEED));
+//             }
             
-            loot.add(potionStack);
-        }
-    }
-}
+//             loot.add(potionStack);
+//         }
+//     }
+// }
