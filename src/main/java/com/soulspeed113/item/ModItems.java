69e454a04930e156842415ec29baa4cd76511336
Videsh Arivazhagan <arivvid27@gmail.com>
2025-05-25 09:11:13 -0700

    Another file

--- src/main/java/com/soulspeed113/item/ModItems.java
+++ src/main/java/com/soulspeed113/item/ModItems.java
@@ -1,10 +1,10 @@
 package com.soulspeed113.item;
 
-import com.soulspeed113.SoulSpeed;
-import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
+// import com.soulspeed113.SoulSpeed;
+// import net.minecraft.item.Item;
+// import net.minecraft.registry.Registries;
+// import net.minecraft.registry.Registry;
+// import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static void registerItems() {
