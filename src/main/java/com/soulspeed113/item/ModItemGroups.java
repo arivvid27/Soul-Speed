648030dfad345ce68b1d7361bf51d1768bc094fd
Videsh Arivazhagan <arivvid27@gmail.com>
2025-05-26 13:21:55 -0700

    Built and added. Hopefully works?

--- src/main/java/com/soulspeed113/item/ModItemGroups.java
+++ src/main/java/com/soulspeed113/item/ModItemGroups.java
@@ -22,59 +22,108 @@ public class ModItemGroups {
             .displayName(Text.translatable("itemgroup.soulspeed113.soul_speed"))
             .icon(() -> {
                 ItemStack stack = new ItemStack(Items.POTION);
-                stack.set(DataComponentTypes.POTION_CONTENTS,
-                new PotionContentsComponent(ModPotions.SOUL_SPEED));
-            return stack;
-        })
-        .entries((displayContext, entries) -> {
-            // POTIONS
+                stack.set(DataComponentTypes.POTION_CONTENTS, 
+                    new PotionContentsComponent(ModPotions.SOUL_SPEED));
+                return stack;
+            })
+            .entries((displayContext, entries) -> {
+                // POTIONS
+                ItemStack soulSpeedPotion = new ItemStack(Items.POTION);
+                soulSpeedPotion.set(DataComponentTypes.POTION_CONTENTS, 
+                    new PotionContentsComponent(ModPotions.SOUL_SPEED));
+                entries.add(soulSpeedPotion);
+
+                ItemStack longSoulSpeedPotion = new ItemStack(Items.POTION);
+                longSoulSpeedPotion.set(DataComponentTypes.POTION_CONTENTS, 
+                    new PotionContentsComponent(ModPotions.LONG_SOUL_SPEED));
+                entries.add(longSoulSpeedPotion);
+
+                ItemStack strongSoulSpeedPotion = new ItemStack(Items.POTION);
+                strongSoulSpeedPotion.set(DataComponentTypes.POTION_CONTENTS, 
+                    new PotionContentsComponent(ModPotions.STRONG_SOUL_SPEED));
+                entries.add(strongSoulSpeedPotion);
+
+                // SPLASH
+                ItemStack splashSoulSpeed = new ItemStack(Items.SPLASH_POTION);
+                splashSoulSpeed.set(DataComponentTypes.POTION_CONTENTS, 
+                    new PotionContentsComponent(ModPotions.SOUL_SPEED));
+                entries.add(splashSoulSpeed);
+
+                ItemStack splashLongSoulSpeed = new ItemStack(Items.SPLASH_POTION);
+                splashLongSoulSpeed.set(DataComponentTypes.POTION_CONTENTS, 
+                    new PotionContentsComponent(ModPotions.LONG_SOUL_SPEED));
+                entries.add(splashLongSoulSpeed);
+
+                ItemStack splashStrongSoulSpeed = new ItemStack(Items.SPLASH_POTION);
+                splashStrongSoulSpeed.set(DataComponentTypes.POTION_CONTENTS, 
+                    new PotionContentsComponent(ModPotions.STRONG_SOUL_SPEED));
+                entries.add(splashStrongSoulSpeed);
+
+                // LINGERING
+                ItemStack lingeringSoulSpeed = new ItemStack(Items.LINGERING_POTION);
+                lingeringSoulSpeed.set(DataComponentTypes.POTION_CONTENTS, 
+                    new PotionContentsComponent(ModPotions.SOUL_SPEED));
+                entries.add(lingeringSoulSpeed);
+
+                ItemStack lingeringLongSoulSpeed = new ItemStack(Items.LINGERING_POTION);
+                lingeringLongSoulSpeed.set(DataComponentTypes.POTION_CONTENTS, 
+                    new PotionContentsComponent(ModPotions.LONG_SOUL_SPEED));
+                entries.add(lingeringLongSoulSpeed);
+
+                ItemStack lingeringStrongSoulSpeed = new ItemStack(Items.LINGERING_POTION);
+                lingeringStrongSoulSpeed.set(DataComponentTypes.POTION_CONTENTS, 
+                    new PotionContentsComponent(ModPotions.STRONG_SOUL_SPEED));
+                entries.add(lingeringStrongSoulSpeed);
+
+                // ARROWS
+                ItemStack arrowSoulSpeed = new ItemStack(Items.TIPPED_ARROW);
+                arrowSoulSpeed.set(DataComponentTypes.POTION_CONTENTS, 
+                    new PotionContentsComponent(ModPotions.SOUL_SPEED));
+                entries.add(arrowSoulSpeed);
+
+                ItemStack arrowLongSoulSpeed = new ItemStack(Items.TIPPED_ARROW);
+                arrowLongSoulSpeed.set(DataComponentTypes.POTION_CONTENTS, 
+                    new PotionContentsComponent(ModPotions.LONG_SOUL_SPEED));
+                entries.add(arrowLongSoulSpeed);
+
+                ItemStack arrowStrongSoulSpeed = new ItemStack(Items.TIPPED_ARROW);
+                arrowStrongSoulSpeed.set(DataComponentTypes.POTION_CONTENTS, 
+                    new PotionContentsComponent(ModPotions.STRONG_SOUL_SPEED));
+                entries.add(arrowStrongSoulSpeed);
+            })
+            .build()
+    );
+
+    public static void registerItemGroups() {
+        SoulSpeed.LOGGER.info("Registering Item Groups for " + SoulSpeed.MOD_ID);
+        
+        ItemGroupEvents.modifyEntriesEvent(net.minecraft.item.ItemGroups.FOOD_AND_DRINK).register(entries -> {
             ItemStack soulSpeedPotion = new ItemStack(Items.POTION);
-            soulSpeedPotion.set(DataComponentTypes.POTION_CONTENTS,
+            soulSpeedPotion.set(DataComponentTypes.POTION_CONTENTS, 
                 new PotionContentsComponent(ModPotions.SOUL_SPEED));
             entries.add(soulSpeedPotion);
 
             ItemStack longSoulSpeedPotion = new ItemStack(Items.POTION);
-            longSoulSpeedPotion.set(DataComponentTypes.POTION_CONTENTS,
+            longSoulSpeedPotion.set(DataComponentTypes.POTION_CONTENTS, 
                 new PotionContentsComponent(ModPotions.LONG_SOUL_SPEED));
             entries.add(longSoulSpeedPotion);
 
             ItemStack strongSoulSpeedPotion = new ItemStack(Items.POTION);
-            strongSoulSpeedPotion.set(DataComponentTypes.POTION_CONTENTS,
+            strongSoulSpeedPotion.set(DataComponentTypes.POTION_CONTENTS, 
                 new PotionContentsComponent(ModPotions.STRONG_SOUL_SPEED));
             entries.add(strongSoulSpeedPotion);
+        });
 
-            // LINGERING
-            ItemStack lingeringSoulSpeed = new ItemStack(Items.LINGERING_POTION);
-            lingeringSoulSpeed.set(DataComponentTypes.POTION_CONTENTS,
+        ItemGroupEvents.modifyEntriesEvent(net.minecraft.item.ItemGroups.COMBAT).register(entries -> {
+            ItemStack splashSoulSpeed = new ItemStack(Items.SPLASH_POTION);
+            splashSoulSpeed.set(DataComponentTypes.POTION_CONTENTS, 
                 new PotionContentsComponent(ModPotions.SOUL_SPEED));
-            entries.add(lingeringSoulSpeed);
+            entries.add(splashSoulSpeed);
 
-            ItemStack lingeringLongSoulSpeed = new ItemStack(Items.LINGERING_POTION);
-            lingeringLongSoulSpeed.set(DataComponentTypes.POTION_CONTENTS,
-                new PotionContentsComponent(ModPotions.LONG_SOUL_SPEED));
-            entries.add(lingeringLongSoulSpeed);
-
-            ItemStack lingeringStrongSoulSpeed = new ItemStack(Items.LINGERING_POTION);
-            lingeringStrongSoulSpeed.set(DataComponentTypes.POTION_CONTENTS,
-                new PotionContentsComponent(ModPotions.STRONG_SOUL_SPEED));
-            entries.add(lingeringStrongSoulSpeed);
-
-            // ARROWS
             ItemStack arrowSoulSpeed = new ItemStack(Items.TIPPED_ARROW);
-            arrowSoulSpeed.set(DataComponentTypes.POTION_CONTENTS,
+            arrowSoulSpeed.set(DataComponentTypes.POTION_CONTENTS, 
                 new PotionContentsComponent(ModPotions.SOUL_SPEED));
             entries.add(arrowSoulSpeed);
-
-            ItemStack arrowLongSoulSpeed = new ItemStack(Items.TIPPED_ARROW);
-            arrowLongSoulSpeed.set(DataComponentTypes.POTION_CONTENTS,
-                new PotionContentsComponent(ModPotions.LONG_SOUL_SPEED));
-            entries.add(arrowLongSoulSpeed);
-
-            ItemStack arrowStrongSoulSpeed = new ItemStack(Items.TIPPED_ARROW);
-            arrowStrongSoulSpeed.set(DataComponentTypes.POTION_CONTENTS,
-                new PotionContentsComponent(ModPotions.STRONG_SOUL_SPEED));
-            entries.add(arrowStrongSoulSpeed);
-        })
-        .build()
-    );
+        });
+    }
 }
